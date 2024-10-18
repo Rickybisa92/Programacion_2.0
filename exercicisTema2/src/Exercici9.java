006770932cce83ca97605cc81bc139f81434bf3d
Rickybisa92 <ricardobji92@gmail.com>
2024-10-18 15:32:01 +0200

    ejercicio9 corregido

--- exercicisTema2/src/Exercici9.java
+++ exercicisTema2/src/Exercici9.java
@@ -7,11 +7,14 @@ i CAF3
 public class Exercici9 {
     public static void main(String[] args) {
         int hexa1 = 0xEE7A;
-        int heax2 = 0xCAF3;
+        int hexa2 = 0xCAF3;
 
 
-        int resultadoAND = hexa1&heax2;
+        int resultadoAND = hexa1 & hexa2;
 
-        System.out.println("El resultado de " + hexa1 + " y " + heax2 + " es: " + resultadoAND);
+        System.out.println(Integer.toBinaryString(hexa1));
+        System.out.println(Integer.toBinaryString(hexa2));
+        System.out.println(Integer.toBinaryString(resultadoAND));
+
     }
 }
