274ad96a9905dccff7786667b6abdd3a946cc95c
Rickybisa92 <ricardobji92@gmail.com>
2024-10-21 12:26:14 +0200

    Programa resuelto

--- Tema3/src/programaCondiciones2.java
+++ Tema3/src/programaCondiciones2.java
@@ -11,8 +11,46 @@ import java.util.Scanner;
 public class programaCondiciones2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Que tipo de vehiculo tienes (1-moto, 2-coche, 3-camion):");
+        System.out.println("Que tipo de vehiculo tienes (1-moto, 2-coche, 3-camion)?:");
         int vehiculo = scanner.nextInt();
+
+        System.out.println("Cuantas horas lleva aparcado?");
+        int horas = scanner.nextInt();
+
+        double tarifa = 0;
+
+        switch (vehiculo) {
+            case 1: //Moto
+                tarifa = 1.50;
+                if (horas > 10 ) {
+                    tarifa += tarifa * 0.30;
+                } else if (horas > 5) {
+                    tarifa += tarifa * 0.10;
+                }
+                break;
+            case 2: //coche
+                tarifa = 3.20;
+                if (horas > 10) {
+                    tarifa += tarifa * 0.30;
+                } else if (horas > 5) {
+                    tarifa += tarifa * 0.15;
+                }
+                break;
+            case 3: //Camion
+                tarifa = 5.50;
+                if (horas > 10) {
+                    tarifa += tarifa * 0.30;
+                } else if (horas > 5) {
+                    tarifa += tarifa * 0.17;
+                }
+                break;
+            default:
+                System.out.println("Tipo de vehiculo no valido.");
+                return;
+        }
+
+        tarifa = Math.round(tarifa * 100.0) / 100.0;
+        System.out.println("El precio del aparcamiento de su vehiculo es: " + tarifa + "€.");
 
     }
 }
