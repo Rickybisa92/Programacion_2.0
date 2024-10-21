c8b8394856bff6f77308c5e3827cdd3f1c72daac
Rickybisa92 <ricardobji92@gmail.com>
2024-10-21 19:17:25 +0200

    cambio variable

--- Tema3/src/programaCondiciones2.java
+++ Tema3/src/programaCondiciones2.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-/*
+
 //Haz un programa que calcule el precio de aparcamiento de un vehiculo
         //se tiene que pedir el tipo de vehiculo (1-moto, 2-coche, 3-camion)
         //cada tipo de vehiculo tiene una tarifa (1,50 moto, 3,20 coche, 5,50 camion)
         //si un vehiculo esta aparcado mas de 5 horas, se aplica una recarga
         // de un 10% = 5 horas la moto, de un 15% = 5h el coche y un (17% = 5h un camion) depende del tipo
         // si esta aparcado mas de 10h se aplica una recarga fija de 30% para todos los vehiculos
- */
+
 public class programaCondiciones2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +17,7 @@ public class programaCondiciones2 {
         System.out.println("Cuantas horas lleva aparcado?");
         int horas = scanner.nextInt();
 
-        double tarifa = 0;
+        double tarifa;
 
         switch (vehiculo) {
             case 1: //Moto
