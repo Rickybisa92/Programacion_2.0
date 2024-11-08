d89c8fb9f29359a8adb64148b560995c9e221079
Rickybisa92 <ricardobji92@gmail.com>
2024-11-08 16:22:12 +0100

    exercici4Done

--- exercicisTurtle/src/Exercici4.java
+++ exercicisTurtle/src/Exercici4.java
@@ -2,36 +2,55 @@ public class Exercici4 {
     public static void main(String[] args) {
         Turtle t = new Turtle(500, 500);
 
-        int numeroHojas = 2;  // Número de hojas o niveles en el dibujo
-        int cateto = 50;      // Longitud del cateto
-        int hipotenusa = 70;  // Longitud de la hipotenusa
+        int numeroHojas = 5;  // Número de hojas o niveles en el dibujo
+        int cateto1 = 40;      // Longitud del cateto
+        int hipotenusa = (int) Math.sqrt((cateto1 * cateto1) + (cateto1 * cateto1));  // Longitud de la hipotenusa
 
 
         // Dibuja el tronco después de las hojas
-        t.forward(cateto / 2);  // Dibuja el tronco
+        t.forward(cateto1 / 2);  // Dibuja el tronco
 
+        t.turnLeft(90);
+        hojasDerecha(t, cateto1, hipotenusa, numeroHojas);
+        t.turnRight(135);
+        t.forward(20);
+        t.turnRight(90);
+        t.forward(20);
+        hojasIzquierda(t, cateto1, hipotenusa,numeroHojas);
+        t.turnRight(45);
+        t.forward(cateto1 / 2);
+        t.turnLeft(90);
 
-
-        for (int i = 0; i < numeroHojas; i++) {
-            dibujaHoja(t, cateto, hipotenusa);
-
-            // Mueve la tortuga hacia abajo para el siguiente nivel
-            t.turnLeft(135);
-            t.forward(cateto);
-            t.turnRight(135);
-
-        }
-
-        t.setDelay(700);
+        t.setDelay(100);
         t.markCursor();
         t.show();
     }
 
-    public static void dibujaHoja(Turtle t, int cateto, int hipotenusa) {
-        t.turnLeft(90);           // Orienta hacia arriba
-        t.forward(cateto);        // Dibuja el primer segmento vertical de la "V"
-        t.turnRight(135);         // Gira hacia la hipotenusa de la "V"
-        t.forward(hipotenusa);    // Dibuja la primera línea diagonal de la "V"
+    public static void hojasDerecha(Turtle t, int cateto, int hipotenusa, int numeroHojas) {
 
+        for (int i = 0; i < numeroHojas; i++) {
+            t.forward(cateto);
+            t.turnRight(135);
+            t.forward(hipotenusa);
+            t.turnLeft(135);
+
+        }
+
+
+    }
+
+    public static void hojasIzquierda(Turtle t, int cateto, int hipotenusa, int numeroHojas) {
+
+        for (int i = 0; i < numeroHojas; i++) {
+            t.forward(hipotenusa);
+            t.turnLeft(225);
+            t.forward(cateto);
+            t.turnRight(225);
+
+
+
+
+
+        }
     }
 }
