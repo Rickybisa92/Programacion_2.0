df0b93d23f7c526205325e90cfed12cce3014ae9
Rickybisa92 <ricardobji92@gmail.com>
2025-02-03 20:33:10 +0100

    comenzando tests transposition

--- practicaXifratge/src/Transposition.java
+++ practicaXifratge/src/Transposition.java
@@ -2,20 +2,40 @@
 public class Transposition {
 
     static String cypher(String s, int dim) {
-        return "";    
+
+        char[] result = new char[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < dim; j++) {
+                if (i % dim == j) {
+                    result[j] = s.charAt(i);
+                }
+
+            }
+
+        }
+
+        return "";
     }
-    
+
+
     static String decypher(String s, int dim) {
+
+        char[] result = new char[s.length()];
+
         return "";
     }
 
 
     static String cypher(String s, String key) {
+
+
         return "";
     }
 
 
     static String decypher(String s, String key) {
+
         return "";
     }
 
