06a9dbada60382d6bdb348d942955b8892fa1c7d
Rickybisa92 <ricardobji92@gmail.com>
2025-02-04 12:27:58 +0100

    primer test transposition pasado

--- practicaXifratge/src/Transposition.java
+++ practicaXifratge/src/Transposition.java
@@ -2,30 +2,84 @@
 public class Transposition {
 
     static String cypher(String s, int dim) {
+        char[][] matriz = matrizCifrado(s, dim);
+        return leerColumnas(matriz, dim);
+    }
 
-        char[] result = new char[s.length()];
-
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < dim; j++) {
-                if (i % dim == j) {
-                    result[j] = s.charAt(i);
-                }
-
-            }
-
+    private static char[][] matrizCifrado(String s, int dim) {
+        int filas = s.length() / dim;
+        if (s.length() % dim != 0) {
+            filas++;
         }
 
-        return "";
+        char[][] matriz = new char[filas][dim];
+        int index = 0;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < dim; j++) {
+                if (index < s.length()) {
+                    matriz[i][j] = s.charAt(index);
+                    index++;
+                } else {
+                    matriz[i][j] = '\0';
+                }
+            }
+        }
+        return matriz;
+    }
+
+    static String leerColumnas(char[][] matriz, int dim) {
+        String result = "";
+        for (int j = 0; j < dim; j++) {
+            for (int i = 0; i < matriz.length; i++) {
+                if (matriz[i][j] != '\0') {
+                    result += matriz[i][j];
+                }
+            }
+        }
+        return result;
     }
 
 
     static String decypher(String s, int dim) {
-
-        char[] result = new char[s.length()];
-
-        return "";
+        char [][] matriz = matrizDescifrado(s, dim);
+        return leerFilas(matriz);
     }
 
+    static char[][] matrizDescifrado(String s, int dim) {
+        int filas = s.length() / dim;
+        if (s.length() % dim != 0) {
+            filas = filas + 1;
+        } else {
+            filas = filas;
+        }
+
+        char[][] matriz = new char[filas][dim];
+        int index = 0;
+        for (int j = 0; j < dim; j++) {
+            for (int i = 0; i < filas; i++) {
+                if (index < s.length()) {
+                    matriz[i][j] = s.charAt(index);
+                    index++;
+                } else {
+                    matriz[i][j] = '\0';
+                }
+            }
+        }
+        return matriz;
+
+    }
+
+    static String leerFilas(char[][] matriz) {
+        String result = "";
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] != '\0') {
+                    result += matriz[i][j];
+                }
+            }
+        }
+        return result;
+    }
 
     static String cypher(String s, String key) {
 
